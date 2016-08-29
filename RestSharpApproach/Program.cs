71c4809a96e14c6736ce822a223e22239f4aae2f
Agustin Camino <agustin.camino@gmail.com>
2016-08-29 09:47:25 -0500

    Add RestSharp.RestClient approach

---
+++ RestSharpApproach/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+
+namespace RestSharpApproach
+{
+    internal class Contributor
+    {
+        public string Login { get; set; }
+        public short Contributions { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Login, 20}: {Contributions} contributions";
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var client = new RestClient("https://api.github.com");
+
+            var request = new RestRequest("repos/twilio/twilio-csharp/contributors", Method.GET);
+            // Add HTTP headers
+            request.AddHeader("User-Agent", "Nothing");
+
+            // Execute the request and automatically deserialize the result.
+            var contributors = client.Execute<List<Contributor>>(request);
+            contributors.Data.ForEach(Console.WriteLine);
+
+            Console.ReadLine();
+        }
+    }
+}
