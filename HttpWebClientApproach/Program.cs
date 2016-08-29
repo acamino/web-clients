569b3c6a39e08d908290c7dcdac2d7b7054728b7
Agustin Camino <agustin.camino@gmail.com>
2016-08-29 09:20:42 -0500

    Add System.Net.HttpWebClient approach

---
+++ HttpWebClientApproach/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace HttpWebClientApproach
+{
+    internal class Contributor
+    {
+        public string Login { get; set; }
+        public short Contributions { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Login, 20}: {Contributions} contributions";
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var webRequest = WebRequest.Create("https://api.github.com/repos/twilio/twilio-csharp/contributors") as HttpWebRequest;
+            if (webRequest == null)
+            {
+                return;
+            }
+
+            webRequest.ContentType = "application/json";
+            webRequest.UserAgent = "Nothing";
+
+            using (var s = webRequest.GetResponse().GetResponseStream())
+            {
+                using (var sr = new StreamReader(s))
+                {
+                    var contributorsAsJson = sr.ReadToEnd();
+                    var contributors = JsonConvert.DeserializeObject<List<Contributor>>(contributorsAsJson);
+                    contributors.ForEach(Console.WriteLine);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
